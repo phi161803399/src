0470a01114d54cfa767b7980cf589d8b87a80c1b
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-07-16 14:57:19 +0200

    PostingInputNotWorking

--- RekenMachineAPI.API/Controllers/CalculatorController.cs
+++ RekenMachineAPI.API/Controllers/CalculatorController.cs
@@ -10,6 +10,16 @@ namespace RekenMachineAPI.API.Controllers
 {
     public class CalculatorController : BaseApiController
     {
-        
+        private readonly CalculationService _calculationService;
+        public CalculatorController(CalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+        [HttpPost, Route]
+        public async Task<IHttpActionResult> Post(string input)
+        {
+            _calculationService.Calculate(input);
+            return Ok();
+        }
     }
 }
