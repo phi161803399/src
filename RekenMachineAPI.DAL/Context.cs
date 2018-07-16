222dd5b6652e95bba8fe7c599d5c85a547fdc998
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-07-16 12:33:44 +0200

    RemoveConfigurationFromContext

--- RekenMachineAPI.DAL/Context.cs
+++ RekenMachineAPI.DAL/Context.cs
@@ -14,50 +14,12 @@ namespace RekenMachineAPI.DAL
         public DbSet<CalculationType> CalculationTypes { get; set; }    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Expression>()
-                .ToTable("Calculations");
-
-//            modelBuilder.Entity<Expression>()
-//                .Ignore(e => e.LeftHand)
-//                .Ignore(e => e.RightHand);
-
-            modelBuilder.Entity<Expression>()
-                .Property(e => e.ExpressionAsString)
-                .HasColumnName("Calculation")
-                .IsRequired()
-                .HasColumnOrder(2);
-
-            modelBuilder.Entity<Expression>()
-                .Property(e => e.DateCreated)
-                .HasColumnName("Date")
-                .HasColumnOrder(5);
-
-            modelBuilder.Entity<Expression>()
-                .Property(e => e.Operation)
-                .HasColumnOrder(3)
-                .HasColumnName("Operation");
-
-            modelBuilder.Entity<Expression>()
-                .Property(e => e.Val)
-                .HasColumnOrder(4)
-                .HasColumnName("Result");
-
-            modelBuilder.Entity<Expression>()
-                .HasRequired(e => e.LeftHand)
-                .WithRequiredDependent();
-
-            modelBuilder.Entity<Expression>()
-                .HasRequired(e => e.RightHand)
-                .WithRequiredDependent();
-
             modelBuilder.Properties<string>()
                 .Configure(configuration => configuration
                     .HasMaxLength(4000));
 
             base.OnModelCreating(modelBuilder);
             modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
-
-//            modelBuilder.Configurations.Add(new EntityTypeConfiguration.ExpressionConfiguration());
         }
     }
 }
